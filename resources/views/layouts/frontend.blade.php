be402858992e4de2f3ea43ee1e403edd37687c09
Nextus Marketing <marketingteamnextus@gmail.com>
2026-04-17 00:00:58 +0530

    d

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -43,6 +43,9 @@
         <link rel="canonical" href="{{ url()->current() }}" />
         <link rel="icon" type="image/png" href="/frontend/my-img/favicon.png">
         @yield('structured_data')
+    
+    <script type="text/javascript">     (function(c,l,a,r,i,t,y){         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);     })(window, document, "clarity", "script", "wcprfeydar"); </script>
+    
     </head>
 <body>
        
